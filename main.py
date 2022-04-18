23d3709b86fc329eb33c1ea87aff9f8dc446f0ec
mediaproducer.ch <froehlr@me.com>
2022-04-18 19:56:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# First Attempt 01
+#print ("Hello World!!!!!!!!!!!!!!!")
+#print ('I am Iron Man')
+#print ("No, I am Tony Stark")
+#print ("No, I am Poppy")
+
+# one print 3 lines
+#print ("""I am Iron Man.
+#No, I am Tony Stark. 
+#No, I am Poppy.""")
+
+# one print
+#print ("I am Iron Man.\n" + "No, I am Tony Stark.\n" + "No, I am Poppy.")
+
+# Poppy ten times
+#print ("I am a Poppy\n" * 10)
+
+# Hashtag
+#name = input ("What is your Name? ")
+name = "Susan"
+print ("\nHello " + name + ". Nice to meet you.\n")
+
+# Hashtag
+#menu = "One, Two, Tree, Four"
+#print (name + ", what would you like to order from our menu today?\n\nThis is what you can order: " + menu)
+
+#choice = input("\nYour choice? ")
+#print ("\nThis is your choice: " + choice)
+
+# Hashtag
+menu = "One, Two, Tree, Four"
+choice = input (name + ", what would you like to order from our menu today?\n\nThis is what you can order: " + menu + ("\nYour choice? "))
+print ("\nThis is your choice: " + choice)
